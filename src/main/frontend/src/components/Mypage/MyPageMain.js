22a9c7c2e6d6bd75eabf7c8a88aeff4fb6ce2f71
Chosh <chosh930911@gmail.com>
2023-07-07 15:22:18 +0900

    20230707/ 마이페이지 context 수정 중(2)

--- src/main/frontend/src/components/Mypage/MyPageMain.js
+++ src/main/frontend/src/components/Mypage/MyPageMain.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback } from "react";
 import { useLocation } from "react-router";
 import MenuBar from "./MypageMenu";
 import styled from "styled-components";
-import MyprofileEdit from "./MyProfileEdit";
 
 const MyPageMain = () => {
     
