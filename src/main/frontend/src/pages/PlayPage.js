343c94761b54cb75001cf9f67d50be16b701ee7e
thkim <thkim1347@gmail.com>
2023-07-20 15:17:01 +0900

    2023-07-20/공연페이지 렌더링 수정/PlayPage.js

--- src/main/frontend/src/pages/PlayPage.js
+++ src/main/frontend/src/pages/PlayPage.js
@@ -8,40 +8,39 @@ import Info from "../components/playPage/Info";
 import styled from "styled-components";
 import PlayInfoApi from "../api/PlayInfoApi";
 import OneReview from "../components/playPage/OneLineReview";
-
 const Contents = styled.div`
     width: 60%;
     position: relative;
-    left: 20%;
-        @media (max-width:2560px) {
-            left: 27.5%;
+    @media (max-width:2560px) {
+        left: 27.5%;
 
-        }
-        @media (max-width:1920px) {
-            left: 20%;
-        }
-        @media (max-width: 768px) {
-            position: unset;
-            width: 100%;
-            overflow: hidden;
-            left: 0;
-        }
+    }
+    @media (max-width:1920px) {
+        left: 20%;
+    }
+    @media (max-width: 768px) {
+        position: unset;
+        width: 100%;
+        overflow: hidden;
+        left: 0;
+    }
+
 `
 const All = styled.div`
     width: 100%;
-
 `
+
 
 const PlayPage = () => {
     const[type,setType] = useState("default");
+    const [showPlayNav, setShowPlayNav] = useState(false); // State 추가
 
 	const handleType = (e) =>{
 		setType(e);
-	}
 
+	}
     const [playInfo,setPlayInfo] = useState(null);
     const playId = localStorage.getItem("playId");
-
     useEffect(()=>{
         const play = async()=>{
             const rsp = await PlayInfoApi.selectPlayInfo(playId);
@@ -50,13 +49,23 @@ const PlayPage = () => {
         play();
     },[])
 
+    useEffect(() => {
+        // 0.5초 뒤에 PlayNav 컴포넌트 보여주기
+        const timeout = setTimeout(() => {
+          setShowPlayNav(true);
+        }, 500);
+
+        // 컴포넌트가 언마운트될 때 타이머를 정리 (cleanup)
+        return () => clearTimeout(timeout);
+      }, []);
+
     return(
-      <All>
+        <All>
             <Header/>
             {playInfo && playInfo.map(play =>(
             <Contents key = {play.playId}>
                 <Info/>
-                <PlayNav  handleType={handleType}/>
+                {showPlayNav && <PlayNav handleType={handleType} />}
                 {type === "default" &&(
                     <Detail/>
                 )}
@@ -69,7 +78,7 @@ const PlayPage = () => {
             </Contents>
             ))}
             <div className="footer">
-                <Footer/>
+                {showPlayNav && <Footer/>}
             </div>
         </All>
     )
